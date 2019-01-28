Using odrpack to fit trends in spectroscopic data sets and to determine the correct coefficients to use for the scaling relation numax values in the probability determination code.

mass_distr.py
@@ -1324,11 +1324,11 @@ if __name__ == '__main__':
     ax2.text(0.4, 0.95, '11 R$_{\odot}$', horizontalalignment='center',verticalalignment='center', transform=ax2.transAxes,fontsize=10)
     ax2.set_xlim(5300,4400)
     ax2.set_ylim(1.65,1.85)
-    hrd.savefig('clump_rads.pdf',bbox_inches='tight')
+    # hrd.savefig('clump_rads.pdf',bbox_inches='tight')
 
-    plt.show()
+    # plt.show()
 
-    sys.exit()
+    # sys.exit()
     APK2_alpha = APK2[APK2['alpha'] > 0.1]
     K2_alpha = AS[AS['alpha'] > 0.1]
 
@@ -1885,40 +1885,40 @@ if __name__ == '__main__':
 
     ''' En masse commenting '''
 #
-#     ''' Radius Distributions '''
-#     APK2_alpha = APK2[APK2['alpha'] > 0.1]
-#     K2_alpha = AS[AS['alpha'] > 0.1]
-#     # TRI3a = TRI[TRI['#Gc'] == 1]
-#     # TRI3b = TRI[TRI['#Gc'] == 2]
-#     # TRI3c = TRI[TRI['#Gc'] == 3]
-#     K2_para = Luca[Luca['parallax_error']/Luca['parallax'] < .1]
-#     f = plt.figure()
-#     plt.hist(APK2['rad'],bins=np.linspace(0,20,100),label=r'APOKASC',color='grey',alpha=0.2,normed=True)
-#
-#     # plt.hist(Kep_Sim['radius'],bins=np.linspace(0,20,100),label=r'APOKASC',color='grey',alpha=0.2,normed=True)
-#     # mdf.histo(APK2_alpha,'rad',np.linspace(0,20,100),r'Radius [R$_{\odot}$]',0,r'$\alpha$-rich Kepler')
-#     # plt.hist(APK2_alpha['rad'],bins=np.linspace(0,20,100),histtype='step',label=r'APOKASC $\alpha$-rich',color='r',normed=True,linewidth=2)
-#     # mdf.histo(AS,'rad',np.linspace(4,20,80),r'Radius [R$_{\odot}$]',0,r'K2 Spec.')
-#
-#     # plt.hist(K2_para['rad'],bins=np.linspace(0,20,100),histtype='step',label=r'K2 SM',normed=True,linewidth=2)
-#     # plt.hist(K2_para['Rgaia'],bins=np.linspace(0,20,50),histtype='step',label=r'K2$_{Gaia}$',normed=True,linewidth=2)
-#
-#     # plt.hist(AS['Rgaia'],bins=np.linspace(0,20,50),histtype='step',label=r'K2 Spec.',normed=True,linewidth=2)
-#     # plt.hist(AS['rad'],bins=np.linspace(0,20,50),histtype='step',label=r'K2 Spec.',normed=True,linewidth=2)
-#     # plt.hist(K2_alpha['rad'],bins=np.linspace(0,20,100),histtype='step',label=r'K2 $\alpha$-rich',normed=True,linewidth=2)
-#     # plt.hist(K2_alpha['Rgaia'],bins=np.linspace(0,20,50),histtype='step',label=r'$\alpha$-rich K2, Gaia',normed=True,linewidth=2)
-#     plt.hist(TRI3a['radius'],bins=np.linspace(0,20,100),histtype='step',label=r'TRI: Thin',normed=True,linewidth=2)
-#     plt.hist(TRI3b['radius'],bins=np.linspace(0,20,100),histtype='step',label=r'TRI: Thick',normed=True,linewidth=2)
-#     # mdf.histo(Luca,'rad',np.linspace(0,20,100),r'Radius [R$_{\odot}$]',0,r'K2')
-#     # mdf.histo(gold,'rad',np.linspace(0,20,100),r'Radius [R$_{\odot}$]',0,r'K2, Gold')
-#     plt.yticks([])
-#     plt.xlim(3.5,18)
-#     plt.xlabel(r'Radius [R$_{\odot}$]',fontsize=15)
-#     plt.legend(prop={'size':10})
-#     # f.savefig('Radius_TRI.pdf', bbox_inches='tight')
-#     # # pdf.savefig(f)
-#     # plt.show()
-#     # sys.exit()
+    ''' Radius Distributions '''
+    APK2_alpha = APK2[APK2['alpha'] > 0.1]
+    K2_alpha = AS[AS['alpha'] > 0.1]
+    # TRI3a = TRI[TRI['#Gc'] == 1]
+    # TRI3b = TRI[TRI['#Gc'] == 2]
+    # TRI3c = TRI[TRI['#Gc'] == 3]
+    K2_para = Luca[Luca['parallax_error']/Luca['parallax'] < .1]
+    f = plt.figure()
+    plt.hist(APK2['rad'],bins=np.linspace(0,20,100),label=r'APOKASC',color='grey',alpha=0.2,normed=True)
+
+    # plt.hist(Kep_Sim['radius'],bins=np.linspace(0,20,100),label=r'APOKASC',color='grey',alpha=0.2,normed=True)
+    # mdf.histo(APK2_alpha,'rad',np.linspace(0,20,100),r'Radius [R$_{\odot}$]',0,r'$\alpha$-rich Kepler')
+    # plt.hist(APK2_alpha['rad'],bins=np.linspace(0,20,100),histtype='step',label=r'APOKASC $\alpha$-rich',color='r',normed=True,linewidth=2)
+    # mdf.histo(AS,'rad',np.linspace(4,20,80),r'Radius [R$_{\odot}$]',0,r'K2 Spec.')
+
+    plt.hist(K2_para['rad'],bins=np.linspace(0,20,100),histtype='step',label=r'K2 SM',normed=True,linewidth=2)
+    plt.hist(K2_para['Rgaia'],bins=np.linspace(0,20,50),histtype='step',label=r'K2$_{Gaia}$',normed=True,linewidth=2)
+
+    # plt.hist(AS['Rgaia'],bins=np.linspace(0,20,50),histtype='step',label=r'K2 Spec.',normed=True,linewidth=2)
+    # plt.hist(AS['rad'],bins=np.linspace(0,20,50),histtype='step',label=r'K2 Spec.',normed=True,linewidth=2)
+    # plt.hist(K2_alpha['rad'],bins=np.linspace(0,20,100),histtype='step',label=r'K2 $\alpha$-rich',normed=True,linewidth=2)
+    # plt.hist(K2_alpha['Rgaia'],bins=np.linspace(0,20,50),histtype='step',label=r'$\alpha$-rich K2, Gaia',normed=True,linewidth=2)
+    # plt.hist(TRI3a['radius'],bins=np.linspace(0,20,100),histtype='step',label=r'TRI: Thin',normed=True,linewidth=2)
+    # plt.hist(TRI3b['radius'],bins=np.linspace(0,20,100),histtype='step',label=r'TRI: Thick',normed=True,linewidth=2)
+    # mdf.histo(Luca,'rad',np.linspace(0,20,100),r'Radius [R$_{\odot}$]',0,r'K2')
+    # mdf.histo(gold,'rad',np.linspace(0,20,100),r'Radius [R$_{\odot}$]',0,r'K2, Gold')
+    plt.yticks([])
+    plt.xlim(3.5,18)
+    plt.xlabel(r'Radius [R$_{\odot}$]',fontsize=15)
+    plt.legend(prop={'size':10})
+    # f.savefig('Radius_TRI.pdf', bbox_inches='tight')
+    # # pdf.savefig(f)
+    # plt.show()
+    # sys.exit()
 #     #
 #     # f, ax = plt.subplots()
 #     # AS = AS[AS['alpha'] > -90]

seismo_pipeline_comps.py
@@ -338,6 +338,7 @@ def comp_seismic_plot(df,comp_err,params,uncerts,labels,seismo):
     df[comp_err] = np.sqrt(df[uncerts[0]]**2+df[uncerts[1]]**2)
     df['Diff'] = ((df[params[0]]-df[params[1]])/df[comp_err])
     df = df[abs(df['Diff']) < 5]
+    df = df.reset_index(drop=True)
     mu = np.mean(df['Diff'])
     std = np.std(df['Diff'])
 
@@ -363,7 +364,7 @@ def comp_seismic_plot(df,comp_err,params,uncerts,labels,seismo):
              facecolor='b',alpha=0.5, label='__nolegend__')
     x = np.linspace(min(df['Diff']),max(df['Diff']),100)
     y = mlab.normpdf(x, mu, std)
-    print(mu, std)
+    print(mu, std, np.mean(df[comp_err]))
     ref = mlab.normpdf(x, 0.0, 1.0)
     ax2.plot(y,x,'r--',linewidth=2, label='Fit')
     ax2.plot(ref,x,'k--',linewidth=2,label='N(0,1)')
@@ -376,19 +377,20 @@ def comp_seismic_plot(df,comp_err,params,uncerts,labels,seismo):
     fig.subplots_adjust(wspace=0.0)
     plt.tight_layout()
     # sys.exit()
-    fig.savefig('/home/bmr135/Dropbox/K2Poles/pop_trends/seismo_comp/C3_'+labels[0]+'_'+labels[1]+'_dnu.pdf',bbox_inches='tight')
-    df = df[df[params[0]]>7.5]
+    # fig.savefig('/home/bmr135/Dropbox/K2Poles/pop_trends/seismo_comp/C3_'+labels[0]+'_'+labels[1]+'_dnu.pdf',bbox_inches='tight')
+    # df = df[df[params[0]]>7.5]
     # df[['EPIC',params[0],uncerts[0],params[1],uncerts[1]]].to_csv('/home/bmr135/seismo_comp/C6_'+labels[0]+'_'+labels[1]+'_dnu.txt',index=False)
     # plt.show()
     # sys.exit()
 
 ''' C3 comparisons '''
-# comp_seismic_plot(YS_C3,'comp_err_nmx',['nmx','Snumax'],['nmx_err','e_Snumax'],[r'BHM',r'A2Z'],r'$\nu_{\rm{max}}$')
-# comp_seismic_plot(YB_C3,'comp_err_nmx',['nmx','Bnumax'],['nmx_err','e_Bnumax'],[r'BHM',r'COR'],r'$\nu_{\rm{max}}$')
-# comp_seismic_plot(BS_C3,'comp_err_nmx',['Bnumax','Snumax'],['e_Bnumax','e_Snumax'],[r'COR',r'A2Z'],r'$\nu_{\rm{max}}$')
-comp_seismic_plot(YS_C3,'comp_err_Dnu',['dnu','SDnu'],['dnu_err','e_SDnu'],[r'BHM',r'A2Z'],r'$\Delta\nu$')
-comp_seismic_plot(YB_C3,'comp_err_Dnu',['dnu','BDnu'],['dnu_err','e_BDnu'],[r'BHM',r'COR'],r'$\Delta\nu$')
-comp_seismic_plot(BS_C3,'comp_err_Dnu',['BDnu','SDnu'],['e_BDnu','e_SDnu'],[r'COR',r'A2Z'],r'$\Delta\nu$')
+print(np.median(YS_C3['nmx_err']),np.median(YS_C3['e_Snumax']),np.median(YB_C3['e_Bnumax']))
+comp_seismic_plot(YS_C3,'comp_err_nmx',['nmx','Snumax'],['nmx_err','e_Snumax'],[r'BHM',r'A2Z'],r'$\nu_{\rm{max}}$')
+comp_seismic_plot(YB_C3,'comp_err_nmx',['nmx','Bnumax'],['nmx_err','e_Bnumax'],[r'BHM',r'COR'],r'$\nu_{\rm{max}}$')
+comp_seismic_plot(BS_C3,'comp_err_nmx',['Bnumax','Snumax'],['e_Bnumax','e_Snumax'],[r'COR',r'A2Z'],r'$\nu_{\rm{max}}$')
+# comp_seismic_plot(YS_C3,'comp_err_Dnu',['dnu','SDnu'],['dnu_err','e_SDnu'],[r'BHM',r'A2Z'],r'$\Delta\nu$')
+# comp_seismic_plot(YB_C3,'comp_err_Dnu',['dnu','BDnu'],['dnu_err','e_BDnu'],[r'BHM',r'COR'],r'$\Delta\nu$')
+# comp_seismic_plot(BS_C3,'comp_err_Dnu',['BDnu','SDnu'],['e_BDnu','e_SDnu'],[r'COR',r'A2Z'],r'$\Delta\nu$')
 
 ''' C6 comparisons '''
 # comp_seismic_plot(YS_C6,'comp_err_nmx',['nmx','Snumax'],['nmx_err','e_Snumax'],[r'BHM',r'A2Z'],r'$\nu_{\rm{max}}$')

spectro_photom_comp.py
@@ -153,9 +153,59 @@ def ransac_fit(df,df1,param,label,f,ax,uncert):
     # print(df['outlier_flag'])
     # plt.savefig('/home/bmr135/spectro_comp/C3_RAVE_GES_'+param[1]+'.png')
 
+def odr_fit(df,df1,param,label,f1,ax,uncert):
+    ''' Using orthogonal distance regression alogrithm to perform the fit '''
+    def f(Par,z):
+        return Par[0]*z + Par[1]
+    mpar, cpar, empar, ecpar = [], [], [], []
+    linear = odrpack.Model(f)
+    mydata = odrpack.RealData(df1[param[1]],df[param[0]],sx=df1[uncert[1]],sy=df[uncert[0]])
+    myodr = odrpack.ODR(mydata, linear, beta0=[1., 0.],maxit=20000)
+    myoutput = myodr.run()
+    mpar.append(myoutput.beta[0])
+    cpar.append(myoutput.beta[1])
+    empar.append(myoutput.sd_beta[0])
+    ecpar.append(myoutput.sd_beta[1])
+    print(np.sqrt(np.diag(myoutput.cov_beta * myoutput.res_var)),[empar,ecpar])
+    Z = np.sqrt(np.diag(myoutput.cov_beta * myoutput.res_var))
+    # myoutput.pprint()
+    # ax.set_xlim(df1[param[1]].min()-f, df1[param[1]].max()+f)
+    # ax.set_xlabel(label[1])
+    # ax.set_ylabel(label[0])
+    # ax.scatter(X, y, color='yellowgreen', label='__nolegend__')
+    lw = 2
+
+    df['pert'] = 0.
+    df1['pert'] = 0.
+    # for i in range(100):
+    ''' Add perturbation to the data to vary the fits '''
+    # for j in range(len(df1)):
+    #     gauss1 = np.random.normal(df1[param[1]].iloc[j],df1[uncert[1]].iloc[j],50)
+    #     value = gauss1[np.random.randint(len(gauss1),size=1)][0]
+    #     df1['pert'].iloc[j] = value
+    #     gauss2 = np.random.normal(df[param[0]].iloc[j],df[uncert[0]].iloc[j],50)
+    #     value2 = gauss2[np.random.randint(len(gauss2),size=1)][0]
+    #     df['pert'].iloc[j] = value2
+    #
+    # X = (df1['pert'])
+    # X = X.values.reshape(-1,1)
+    # y = (df['pert'])
+
+    line_X = np.arange(min(df1[param[1]])-f1, max(df1[param[1]])+f1, 0.01)#[:, np.newaxis]
+    ax.plot(line_X,line_X,color='r', linewidth=3,alpha=0.5,linestyle='--',label='__nolegend__')
+    ax.plot(line_X, mpar[0]*line_X + cpar[0], color='orange', linewidth=3, label=r'%.5s$*x$ + %.5s'%(mpar[0],cpar[0]))
+    ax.plot(line_X, (mpar[0]+Z[0])*line_X + (cpar[0]), color='grey', linewidth=3, label=r'$+\sigma$')
+    ax.plot(line_X, (mpar[0]-Z[0])*line_X + (cpar[0]), color='grey', linewidth=3, label=r'$-\sigma$')
+    ax.errorbar(df1[param[1]], df[param[0]], xerr=df1[uncert[1]], yerr=df[uncert[0]], color='blue', marker='.', label='__nolegend__',fmt='o',alpha=0.25)
+    ax.scatter(df1[param[1]], df[param[0]], color='blue', label='__nolegend__')
+    ax.legend(frameon=False)
+
+
+
 if __name__ == "__main__":
     ''' Read in Data '''
     ext = '/media/bmr135/SAMSUNG/GA/K2Poles'
+    # ext = '/media/ben/SAMSUNG/GA/K2Poles'
     A3 = pd.read_csv(ext+'/APO_LAMOST/APOGEE_full_C3.csv')
     A3 = A3.dropna(subset=['TEFF','LOGG','FE_H'])
     A6 = pd.read_csv(ext+'/APO_LAMOST/APOGEE_full_C6.csv')
@@ -270,21 +320,27 @@ if __name__ == "__main__":
     ax5.set_xlabel(r'[Fe/H] - GES, C3',fontsize=15)
 
     if len(ag) > 10:
-        ransac_fit(ag,ga,['TEFF','TEFF'],[r'$T_{\rm{eff}}$ - APOGEE, C3',r'$T_{\rm{eff}}$ - GES, C3'],500,ax,['TEFF_ERR','sig_Teff'])#r'$\Delta(T_{\rm{eff}})_{APO-GES}$'],10)
+        # ransac_fit(ag,ga,['TEFF','TEFF'],[r'$T_{\rm{eff}}$ - APOGEE, C3',r'$T_{\rm{eff}}$ - GES, C3'],500,ax,['TEFF_ERR','sig_Teff'])#r'$\Delta(T_{\rm{eff}})_{APO-GES}$'],10)
+        odr_fit(ag,ga,['TEFF','TEFF'],[r'$T_{\rm{eff}}$ - APOGEE, C3',r'$T_{\rm{eff}}$ - GES, C3'],500,ax,['TEFF_ERR','sig_TEFF'])#r'$\Delta(T_{\rm{eff}})_{APO-GES}$'],10)
         # plt.show()
-        ransac_fit(ag,ga,['LOGG','LOGG'],[r'log$_{10}$(g) - APOGEE, C3',r'log$_{10}$(g) - GES, C3'],0.45,ax1,['LOGG_ERR','sig_logg'])#r'$\Delta($log$_{10}$(g)$)_{APO-GES}$'],0.1)
+        # ransac_fit(ag,ga,['LOGG','LOGG'],[r'log$_{10}$(g) - APOGEE, C3',r'log$_{10}$(g) - GES, C3'],0.45,ax1,['LOGG_ERR','sig_logg'])#r'$\Delta($log$_{10}$(g)$)_{APO-GES}$'],0.1)
+        odr_fit(ag,ga,['LOGG','LOGG'],[r'log$_{10}$(g) - APOGEE, C3',r'log$_{10}$(g) - GES, C3'],0.45,ax1,['LOGG_ERR','sig_LOGG'])#r'$\Delta($log$_{10}$(g)$)_{APO-GES}$'],0.1)
         # plt.show()
-        ransac_fit(ag,ga,['FE_H','FEH'],[r'[Fe/H] - APOGEE, C3',r'[Fe/H] - GES, C3'],1.,ax2,['FE_H_ERR','sig_feh'])#r'$\Delta($[Fe/H]$)_{APO-GES}$'],0.1)
+        # ransac_fit(ag,ga,['FE_H','FEH'],[r'[Fe/H] - APOGEE, C3',r'[Fe/H] - GES, C3'],1.,ax2,['FE_H_ERR','sig_feh'])#r'$\Delta($[Fe/H]$)_{APO-GES}$'],0.1)
+        odr_fit(ag,ga,['FE_H','FEH'],[r'[Fe/H] - APOGEE, C3',r'[Fe/H] - GES, C3'],1.,ax2,['FE_H_ERR','sig_FEH'])#r'$\Delta($[Fe/H]$)_{APO-GES}$'],0.1)
         # plt.show()
         # sys.exit()
 
     ''' RAVE vs Gaia-ESO - C3 '''
     if len(rg) > 10:
-        ransac_fit(rg,gr,['Teff_RAVE','TEFF'],[r'$T_{\rm{eff}}$ - RAVE, C3',r'$T_{\rm{eff}}$ - GES, C3'],500,ax3,['sig_Teff','sig_Teff'])#,r'$\Delta(T_{\rm{eff}})_{RAVE-GES}$'],10)
+        # ransac_fit(rg,gr,['Teff_RAVE','TEFF'],[r'$T_{\rm{eff}}$ - RAVE, C3',r'$T_{\rm{eff}}$ - GES, C3'],500,ax3,['sig_Teff','sig_Teff'])#,r'$\Delta(T_{\rm{eff}})_{RAVE-GES}$'],10)
+        odr_fit(rg,gr,['Teff_RAVE','TEFF'],[r'$T_{\rm{eff}}$ - RAVE, C3',r'$T_{\rm{eff}}$ - GES, C3'],500,ax3,['sig_Teff','sig_TEFF'])#,r'$\Delta(T_{\rm{eff}})_{RAVE-GES}$'],10)
         # plt.show()
-        ransac_fit(rg,gr,['logg_RAVE','LOGG'],[r'log$_{10}$(g) - RAVE, C3',r'log$_{10}$(g) - GES, C3'],0.45,ax4,['sig_logg','sig_logg'])#,r'$\Delta($log$_{10}$(g)$)_{RAVE-GES}$'],0.1)
+        # ransac_fit(rg,gr,['logg_RAVE','LOGG'],[r'log$_{10}$(g) - RAVE, C3',r'log$_{10}$(g) - GES, C3'],0.45,ax4,['sig_logg','sig_logg'])#,r'$\Delta($log$_{10}$(g)$)_{RAVE-GES}$'],0.1)
+        odr_fit(rg,gr,['logg_RAVE','LOGG'],[r'log$_{10}$(g) - RAVE, C3',r'log$_{10}$(g) - GES, C3'],0.45,ax4,['sig_logg','sig_LOGG'])#,r'$\Delta($log$_{10}$(g)$)_{RAVE-GES}$'],0.1)
         # plt.show()
-        ransac_fit(rg,gr,['[Fe/H]_RAVE','FEH'],[r'[Fe/H] - RAVE, C3',r'[Fe/H] - GES, C3'],1.,ax5,['sig_feh','sig_feh'])#,r'$\Delta($[Fe/H]$)_{RAVE-GES}$'],0.1)
+        # ransac_fit(rg,gr,['[Fe/H]_RAVE','FEH'],[r'[Fe/H] - RAVE, C3',r'[Fe/H] - GES, C3'],1.,ax5,['sig_feh','sig_feh'])#,r'$\Delta($[Fe/H]$)_{RAVE-GES}$'],0.1)
+        odr_fit(rg,gr,['[Fe/H]_RAVE','FEH'],[r'[Fe/H] - RAVE, C3',r'[Fe/H] - GES, C3'],1.,ax5,['sig_feh','sig_FEH'])#,r'$\Delta($[Fe/H]$)_{RAVE-GES}$'],0.1)
         # plt.show()
 
     ax3.set_xlim(4400, 5200)
@@ -304,4 +360,4 @@ if __name__ == "__main__":
     #     ransac_fit(al,la,['TEFF','teff_L'],[r'$T_{\rm{eff}}$ - APOGEE, C6',r'$\Delta(T_{\rm{eff}})_{APO-LAMOST}$'],10)
     #     ransac_fit(al,la,['LOGG','logg_L'],[r'log$_{10}$(g) - APOGEE, C6',r'$\Delta($log$_{10}$(g)$)_{APO-LAMOST}$'],0.1)
     #     ransac_fit(al,la,['FE_H','feh_L'],[r'[Fe/H] - APOGEE, C6',r'$\Delta($[Fe/H]$)_{APO-LAMOST}$'],0.1)
-    # plt.show()
+    plt.show()